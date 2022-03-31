8c446a4fee791eb13e58f01a401a30153fe1a9c5
ahmadbisbaih <91840065+ahmadbisbaih@users.noreply.github.com>
2022-03-31 13:48:53 +0300

    Initial Commit

---
+++ MyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTask
+//
+//  Created by Ahmad Isbaih on 31/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
